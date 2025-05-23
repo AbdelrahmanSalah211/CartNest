ee1e26cf8386583861aef0d0e34e3217e239ae7f
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-05-24 00:44:14 +0300

    feat: user, product, and order models

---
+++ backend/models/orderModel.ts
@@ -0,0 +1,41 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+import User from "./userModel";
+import Product from "./productModel";
+import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+enum OrderStatus {
+  APPROVED = "approved",
+  PENDING = "pending",
+  DELIVERED = "delivered",
+}
+
+@Entity("orders")
+class Order extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  quantity: number;
+
+  @Column({ default: new Date(Date.now()) })
+  orderDate: Date;
+
+  @Column({
+    type: "enum",
+    enum: OrderStatus,
+    default: OrderStatus.PENDING,
+  })
+  status: OrderStatus;
+
+  @ManyToOne(() => User, (user) => user.orders)
+  user: User;
+
+  @ManyToMany(() => Product, (product) => product.orders)
+  @JoinTable({
+    name: "orders_products"
+  })
+  products: Product[];
+}
+
+export default Order;
---
+++ backend/models/productModel.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToMany } from "typeorm";
+import Order from "./orderModel";
+import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+@Entity("products")
+class Product extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  body: string;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  price: number;
+
+  @Column()
+  image: string;
+
+  @Column()
+  deleteURL: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  user: number;
+
+  @ManyToMany(() => Order, (order) => order.products)
+  orders: Order[];
+}
+
+export default Product;
