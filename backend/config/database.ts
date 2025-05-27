a460c199219f77aa77afe2f299f29ef27c422d65
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-05-27 11:10:43 +0300

    refactor: export DataSource instance and initialization outside function

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -5,26 +5,27 @@ import Product from "../models/productModel";
 import Order from "../models/orderModel";
 import { UserSubscriber } from "../models/userModel";
 
-async function dbConnection() {
-  const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    poolSize: 5,
-    synchronize: true,
-    logging: true,
-    entities: [
-      User,
-      Product,
-      Order,
-    ],
-    subscribers: [
-      UserSubscriber,
-    ],
-  })
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  poolSize: 5,
+  synchronize: true,
+  logging: true,
+  entities: [
+    User,
+    Product,
+    Order,
+  ],
+  subscribers: [
+    UserSubscriber,
+  ],
+})
+
+async function dbConnection(): Promise<DataSource> {
   return AppDataSource.initialize();
 }
 
